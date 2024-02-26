a69aa054679db24291a3cdcf0adcfa4a3b0a0cc4
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 17:01:04 -0600

    dump into docker install along w/ other post install  steps

--- vms/shared/get-docker.sh
+++ vms/shared/get-docker.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-# convenience script
+# *** convenience install ***
 curl -fsSL https://get.docker.com -o install-docker.sh
 # sudo sh install-docker.sh --dry-run # FYI
 sudo sh install-docker.sh
 
 
 
-# post-install steps
+# *** post-install ***
 sudo usermod -aG docker vagrant
 
 sudo systemctl enable docker.service containerd.service
 sudo systemctl start docker.service containerd.service
 
 
+
+# *** completions ***
+#   (generated vs manual) => fish generated are currently superior
+docker completion fish > /home/vagrant/.config/fish/completions/docker.fish
