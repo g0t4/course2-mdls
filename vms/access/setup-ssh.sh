d16ff6b681f28cc9fa3e6c664e337afe0a533f7d
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 18:58:45 -0600

    notes

--- vms/access/setup-ssh.sh
+++ vms/access/setup-ssh.sh
@@ -4,10 +4,20 @@
 # - no longer have to `cd ubuntu; vagrant ssh` (slow)
 # - instead `ssh ubuntu` anywhere (fast, especially w/ persistent connections)
 
+# understand what is going on:
+#   cd ubuntu; vagrant ssh
+#   now,
+#      vagrant ssh-config
+#      vagrant ssh-config > .\vagrant_config
+#      ssh -F .\vagrant_config ubuntu
+#      mv .\vagrant_config ~/.ssh/config.d/vagrants/ubuntu
+#        ensure ~/.ssh/config has line: Include config.d/vagrants/*
+#      ssh ubuntu
+
 # FYI manually ensure line exists:
 # echo "Include config.d/vagrants/*" >> ~/.ssh/config
 
-ssh_vagrants=~/.ssh/config.d/vagrants
+ssh_vagrants="$HOME/.ssh/config.d/vagrants"
 mkdir -p $ssh_vagrants
 
 echo "generating ssh config... be patient, this takes 5-10 seconds"
