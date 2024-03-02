9173c6f71682bcdaa00a3fd734771d65270d4a9a
Wes Higbee <wes.mcclure@gmail.com>
2024-03-02 00:06:38 -0600

    old note

--- vms/updates/scripts/2-containers.sh
+++ vms/updates/scripts/2-containers.sh
@@ -4,11 +4,12 @@ echo
 echo "before 2-containers:"
 docker container ps -a
 
-# emphermal container:
+
 docker container run -d -p "10000:10000" --name=web envoyproxy/envoy:dev
 docker container run -d -p "10001:10000" --name=web-rm --rm envoyproxy/envoy:dev
 docker container run -d -p "10002:10000" --name=web-restarts --restart=always envoyproxy/envoy:dev
 # FYI restart policy: https://docs.docker.com/reference/cli/docker/container/run/#restart
+
 
 echo
 echo "after 2-containers:"
